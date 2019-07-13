256982e9342ee5c10c7bb0da86d38fda30cd1fe4
Talha Hasan Zia <talhahasanzia@hotmail.com>
2019-07-13 23:41:22 +0400

    Update UserServiceTest.java

--- UserServiceTest.java
+++ UserServiceTest.java
@@ -10,21 +10,36 @@ import static org.junit.Assert.assertTrue;
 public class UserServiceTest {
 
     private UserService userService;
-
+    // Arrange
     @Mock
     Request request;
 
     @Mock
     UserRepo userRepo;
+    
+    @Mock
+    User user;
 
     @Before
     public void setup() {
         userService = new UserService(request, userRepo);
     }
-
+    //------------------------------------------------------//
+    
     @Test
     public void testValidAge_inputPositiveNumber_shouldValidateSuccess() {
+        // act
         boolean result = userService.validateAge(18);
+        // assert
         assertTrue("Expected to validate positive number as valid age but failed", result);
+    }
+    
+    @Test
+    public void testCreateUser_validUserProvided_shouldCallRequestExecuteWithUser(){
+        // act
+        userService.createUser(user);
+        // assert
+        Mockito.verify(request).execute(user, userService);
+        
     }
 }
